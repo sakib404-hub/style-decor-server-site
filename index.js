f8f3452463aa4323bb966cc12f31436f7f20ce67
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-12-16 00:54:23 +0600

    user get and the post api

--- index.js
+++ index.js
@@ -22,12 +22,51 @@ const run = async () => {
   try {
     //connecting with the mongo db and checking if the connection is successful
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    //creating the database and the collection
+    const db = client.db("style-decor-db");
+    const usersCollection = db.collection("users");
+
+    //!USERS RELATED APIS
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      try {
+        const newUser = req.body;
+        const userEmail = newUser.userEmail;
+        const userExist = await usersCollection.findOne({
+          userEmail: userEmail,
+        });
+
+        if (userExist) {
+          return res.status(409).send({ message: "User already exists" });
+        }
+        newUser.createdAt = new Date();
+        newUser.updatedAt = new Date();
+
+        const result = await usersCollection.insertOne(newUser);
+
+        res.status(201).send({ message: "User Created" }, result);
+      } catch (error) {
+        res.status(500).send({ message: error.message });
+      }
+    });
+
+    //? CHECKING IF THE CONNECTION IS MADE WITH THE MONGODB
   } catch (error) {
-    console.log(error);
+    res.status(503).send("Database Unavailable, Connection Failed!");
   }
 };
 run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Hellow World!");
+});
+
+app.listen(port, () => {
+  console.log("This app is Listening from Port Number : ", port);
+});
