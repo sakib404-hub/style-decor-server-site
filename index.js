d6fb3fb4bd3f05512f1cb8af32c3ed9bae34c1df
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-12-15 12:38:15 +0600

    connected mongodb for the database

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5016;
 require("dotenv").config();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// mongoDB connection
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@crud-operation.iftbw43.mongodb.net/?appName=CRUD-operation`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+const run = async () => {
+  try {
+    //connecting with the mongo db and checking if the connection is successful
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+run().catch(console.dir);
